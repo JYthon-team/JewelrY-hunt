45e105b25960c4d68eb1a4c0159d6e9ca2557883
Jade Cestari <Jade_cestari@hotmail.com>
2025-10-05 14:02:22 -0300

    Update teste_mapa.c

--- teste_mapa.c
+++ teste_mapa.c
@@ -32,7 +32,7 @@ int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG);
 
-    SDL_Window* win = SDL_CreateWindow("Mapa SDL2 com textura no chão",
+    SDL_Window* win = SDL_CreateWindow("Mapa SDL2",
                                        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                        640, 480, 0);
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
